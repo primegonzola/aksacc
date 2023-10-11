b03f154aedf885085630c466fce8f07a0f830b79
heretic <info@heretic.com>
2023-10-11 12:15:40 +0200

    fixes

--- src/get-attestation-report.sh
+++ src/get-attestation-report.sh
@@ -14,9 +14,9 @@ if [[ "${ATTESTATION_TYPE}" != "azurevm" ]]; then
     echo "Failure: Attestation type is ${ATTESTATION_TYPE} and not azurevm"
     exit 1
 fi
-ISOLATION_ATTESTATION_TYPE=$(cat /logs/attestation_output_2.log | jq -r '."x-ms-isolation-tee"."x-ms-attestation-type"')
-if [[ "${ISOLATION_ATTESTATION_TYPE}" != "sevsnpvm" ]]; then
-    # can't be
-    echo "Failure: Attestation type is ${ISOLATION_ATTESTATION_TYPE} and not sevsnpvm"
-    exit 1
-fi
+# ISOLATION_ATTESTATION_TYPE=$(cat /logs/attestation_output_2.log | jq -r '."x-ms-isolation-tee"."x-ms-attestation-type"')
+# if [[ "${ISOLATION_ATTESTATION_TYPE}" != "sevsnpvm" ]]; then
+#     # can't be
+#     echo "Failure: Attestation type is ${ISOLATION_ATTESTATION_TYPE} and not sevsnpvm"
+#     exit 1
+# fi
