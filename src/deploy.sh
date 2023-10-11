3721fa66ffb555b00ba166aa8bb82954fd59b8ea
heretic <info@heretic.com>
2023-10-11 11:15:18 +0200

    fixes

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,7 +2,9 @@
 AKS_CLUSTER_NAME="${1}"
 ASK_POOL_NAME="${2}"
 ACR_NAME="${3}"
-ATTESTATION_REPORTER_NAME="acc-attestation-reporter"
+
+APP_IMAGE_NAME="acc-sample-webapp"
+ATTESTATION_IMAGE_NAME="acc-attestation-reporter"
 
 # clean up
 rm -rf ../cccvma
@@ -16,14 +18,21 @@ pushd .. &&
     popd
 
 # lets prepare cvm attestation image
-pushd ../cccvma && \
-cp ../src/cvm-attestation.Dockerfile ./aks-linux-sample && \
-docker build -f ./aks-linux-sample/cvm-attestation.Dockerfile -t $ACR_NAME.azurecr.io/acc-attestation-reporter:latest . && \
-az acr login --name $ACR_NAME && \
-docker push $ACR_NAME.azurecr.io/acc-attestation-reporter:latest && \
-popd
+pushd ../cccvma &&
+    cp ../src/cvm-attestation.Dockerfile ./aks-linux-sample &&
+    docker build -f ./aks-linux-sample/cvm-attestation.Dockerfile -t $ACR_NAME.azurecr.io/${ATTESTATION_IMAGE_NAME}:latest . &&
+    az acr login --name $ACR_NAME &&
+    docker push $ACR_NAME.azurecr.io/${ATTESTATION_IMAGE_NAME}:latest &&
+    popd
 
 # lets prepare web app image
-docker build -f webapp.Dockerfile -t $ACR_NAME.azurecr.io/acc-sample-webapp:latest . && \
-az acr login --name $ACR_NAME && \
-docker push $ACR_NAME.azurecr.io/acc-sample-webapp:latest
+docker build -f webapp.Dockerfile -t $ACR_NAME.azurecr.io/acc-sample-webapp:latest . &&
+    az acr login --name $ACR_NAME &&
+    docker push $ACR_NAME.azurecr.io/acc-sample-webapp:latest
+
+# lets deploy the app with attestation as init container
+cat ./init.yaml | sed \
+    -e "s|<ATTESTATION_IMAGE_NAME>|${ATTESTATION_IMAGE_NAME}|" \
+    -e "s|<APP_IMAGE_NAME>|${APP_IMAGE_NAME}|" | kubectl apply -f -
+
+# check if running
