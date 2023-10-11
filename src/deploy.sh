0a0b7ff3e5a33a7e40e0be66ed846f09e05999b5
heretic <info@heretic.com>
2023-10-11 11:32:44 +0200

    fixes

--- src/deploy.sh
+++ src/deploy.sh
@@ -4,6 +4,7 @@ ASK_POOL_NAME="${2}"
 AKS_NAMESPACE="${3}"
 ACR_NAME="${4}"
 
+APP_NAME="acc-sample-webapp"
 APP_IMAGE_NAME="acc-sample-webapp"
 APP_IMAGE_NAME_FQDN=${ACR_NAME}.azurecr.io/${APP_IMAGE_NAME}:latest
 ATTESTATION_IMAGE_NAME="acc-attestation-reporter"
@@ -23,6 +24,7 @@ pushd .. &&
 # lets prepare cvm attestation image
 pushd ../cccvma &&
     cp ../src/cvm-attestation.Dockerfile ./aks-linux-sample &&
+    cp ../src/get-attestation-report.sh . &&
     docker build -f ./aks-linux-sample/cvm-attestation.Dockerfile -t $ACR_NAME.azurecr.io/${ATTESTATION_IMAGE_NAME}:latest . &&
     az acr login --name $ACR_NAME &&
     docker push $ACR_NAME.azurecr.io/${ATTESTATION_IMAGE_NAME}:latest &&
@@ -41,9 +43,14 @@ kubectl create namespace ${AKS_NAMESPACE}
 
 # lets deploy the app with attestation as init container
 cat ./init.yaml | sed \
-    -e "s|<ATTESTATION_IMAGE_NAME>|${ATTESTATION_IMAGE_NAME_FQDN}|" \
-    -e "s|<APP_IMAGE_NAME>|${APP_IMAGE_NAME_FQDN}|" | kubectl apply -n ${AKS_NAMESPACE} -f -
+    -e "s|<APP_NAME>|${APP_NAME}|" \
+    -e "s|<APP_IMAGE_NAME>|${APP_IMAGE_NAME_FQDN}|" \
+    -e "s|<ATTESTATION_IMAGE_NAME>|${ATTESTATION_IMAGE_NAME_FQDN}|" | kubectl apply -n ${AKS_NAMESPACE} -f -
 
 # check if service and pod are running
 kubectl get service -n ${AKS_NAMESPACE}
 kubectl get pods -n ${AKS_NAMESPACE}
+
+# inspect locally if needed
+echo "To debug locally execute following command:\n"
+echo "kubectl exec --stdin --tty ${APP_NAME}  -n ${AKS_NAMESPACE} -- /bin/bash"
